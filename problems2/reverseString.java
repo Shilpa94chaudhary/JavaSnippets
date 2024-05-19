4228ee7935c63b04a473afe9438fadf6979e24a9
ShilpaGamezop <shilpa.94chaudhary@gmail.com>
2024-05-19 09:46:08 +0530

    HashMap

---
+++ problems2/reverseString.java
@@ -0,0 +1,18 @@
+package problems2;
+
+import java.util.Scanner;
+
+public class reverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String string = sc.nextLine();
+        String reverse = "";
+
+        for (int i = 0; i < string.length(); i++) {
+            reverse = string.charAt(i) + reverse;
+        }
+        System.out.println("Original String: " + string);
+        System.out.println("Reverse String: " + reverse);
+        sc.close();
+    }
+}
